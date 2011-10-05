f5d0477d46ab50e7a9968dde7b6af10c1c2bec7f
brickner <rgb@seanet.com>
2011-10-04 21:35:46 -0700

    This is an initial commit of a Rails app generated in Eclipse.  No modifications have been made to the app after generating the code.

---
+++ InitialRailsApp/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5090bace1ef3b72d4afa0baf410e00525bb96112855e44edcb8f6f34fb4423021443a6b4cda75603d8ac33f88792462dda7997a166e6a93a81711358e41aafe1'
