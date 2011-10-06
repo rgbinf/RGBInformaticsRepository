0c9fa2ccafd33c5662449c6abcbaaeef97e9a7e6
brickner <rgb@seanet.com>
2011-10-05 17:42:17 -0700

    This version of the blog project includes all modifications from tutorial up through and including Section 6.

--- InitialRailsApp/blog/config/routes.rb
+++ InitialRailsApp/blog/config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+  resources :posts
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -47,8 +51,9 @@ Blog::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+    root :to => 'home#index'
+  # just remember to delete public/index.html.
 
   # See how all your routes lay out with "rake routes"
 
